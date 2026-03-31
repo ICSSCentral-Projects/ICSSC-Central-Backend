b64b73327537314c0090d5ad764d999657734433
unknown <valdezjamaica285@gmail.com>
2026-04-01 06:13:47 +0800

    fix: update cloudinary plugin configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,15 @@
-module.exports = ({ env }) => ({
+export default ({ env }: { env: any }) => ({
   upload: {
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('djfwceo4q'),
+         cloud_name: env('djfwceo4q'),
         api_key: env('843748867399732'),
         api_secret: env('TAxifivfxSZ0ktyDaVFBy15GYaU'),
       },
       actionOptions: {
         upload: {},
+        uploadStream: {},
         delete: {},
       },
     },
