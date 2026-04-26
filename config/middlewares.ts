33d76d811e3ac10e472048ccf431fa0bd77a6679
Maica <143175358+j0018@users.noreply.github.com>
2026-04-27 07:21:48 +0800

    Fix formatting of CORS middleware configuration

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -18,16 +18,17 @@ const config: Core.Config.Middlewares = [
     },
   },
   {
-  name: 'strapi::cors',
-  config: {
-    origin: [
-  'https://icsscentral.netlify.app',
-  'https://icsscentral.vercel.app',
-],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization'],
-  },
-},
+  {
+      name: 'strapi::cors',
+      config: {
+        origin: [
+          'https://icsscentral.netlify.app',
+          'https://icsscentral.vercel.app',
+        ],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+        headers: ['Content-Type', 'Authorization'],
+      },
+    },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
